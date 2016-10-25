473c30e49d8924ca1187dfc4620dd5f26638f379
Alexey Melezhik <melezhik@gmail.com>
2016-10-25 14:13:22 +0300

    grep -q bug

--- story.bash
+++ story.bash
@@ -1,9 +1,9 @@
 \curl -L http://install.perlbrew.pl | bash
 
-if grep -q 'source' ~/.bash_profile | grep '~/perl5/perlbrew/etc/bashrc'; then
+if grep 'source' ~/.bashrc | grep -q '~/perl5/perlbrew/etc/bashrc'; then
 :
 else
-  echo 'source ~/perl5/perlbrew/etc/bashrc' >> ~/.bash_profile
+  echo 'source ~/perl5/perlbrew/etc/bashrc' >> ~/.bashrc
 fi
 
 source ~/perl5/perlbrew/etc/bashrc
